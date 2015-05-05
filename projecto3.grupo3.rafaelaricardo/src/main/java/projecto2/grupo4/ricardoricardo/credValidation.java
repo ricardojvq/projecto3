e5688a123b4d7f977958c611a1dd22350ad42f48
Ricardo Quirino <ricardojvquirino@gmail.com>
2015-05-05 12:34:51 +0100

    Login e barra de navegação na calculadora

--- projecto3.grupo3.rafaelaricardo/src/main/java/projecto2/grupo4/ricardoricardo/credValidation.java
+++ projecto3.grupo3.rafaelaricardo/src/main/java/projecto2/grupo4/ricardoricardo/credValidation.java
@@ -20,6 +20,7 @@ public class credValidation implements Serializable {
 	private String username;
 	private String password;
 	private String result = "";
+	private boolean errorMsg = false;
 
 	public credValidation() {
 		users = new ConcurrentHashMap<String, String>();
@@ -54,14 +55,31 @@ public class credValidation implements Serializable {
 	public String doLogin() {
 		if (users.containsKey(username)) {
 			if (password.equals(users.get(username))) {
+				errorMsg = false;
 				result = "calc1.xhtml?faces-redirect=true";
+				username = "";
+				password = "";
 			} else {
 				result = "Password inválida";
+				errorMsg = true;
 			}
 		} else {
 			result = "Username inexistente";
+			errorMsg = true;
 		}
 		return result;
+	}
+	
+	public void doLogout() {
+		
+	}
+
+	public boolean isErrorMsg() {
+		return errorMsg;
+	}
+
+	public void setErrorMsg(boolean errorMsg) {
+		this.errorMsg = errorMsg;
 	}
 
 	public String getResult() {
@@ -70,12 +88,17 @@ public class credValidation implements Serializable {
 
 	public void newUser() {
 		if (users.containsKey(username)) {
+			errorMsg = true;
 			result = "Já existente!";
 		} else {
 			if (password != null) {
 				users.put(username, password);
+				username = "";
+				password = "";
+				errorMsg = true;
 				result = "Utilizador criado com sucesso!";
 			} else {
+				errorMsg = true;
 				result = "Password inválida";
 			}
 		}
