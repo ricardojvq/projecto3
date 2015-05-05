063736ce8d65c9571b6be8a7537ebc68ceaca154
Ricardo Quirino <ricardojvquirino@gmail.com>
2015-05-05 21:36:23 +0100

    Adição do filtro para redireccionar um utilizador já logado para a página da calculadora caso aceda à pagina inicial: login.xhtml

---
+++ projecto3.grupo3.rafaelaricardo/src/main/java/projecto2/grupo4/ricardoricardo/LoginRedirect.java
@@ -0,0 +1,37 @@
+package projecto2.grupo4.ricardoricardo;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class LoginRedirect implements Filter
+{
+	private FilterConfig customedFilterConfig;
+
+	public void init(FilterConfig customedFilterConfig) throws ServletException {
+		this.customedFilterConfig = customedFilterConfig;
+	}
+
+	public void doFilter(ServletRequest req, ServletResponse resp,FilterChain chain) throws IOException, ServletException
+	{
+
+		if (((HttpServletRequest)req).getSession().getAttribute(LoggedUser.AUTH_KEY) != null) {
+			((HttpServletResponse)resp).sendRedirect("/Projecto3/Authorized/calc1.xhtml");
+		} else {
+			chain.doFilter(req, resp);
+		}
+	}
+
+	public void destroy()
+	{
+		customedFilterConfig = null;
+	}
+}
